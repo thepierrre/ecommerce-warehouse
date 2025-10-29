65145e1badf8e9fd56386e11567e77f0ebe1778e
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-10-29 18:17:31 +0100

    add routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,13 +1,27 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+
+// Health & Monitoring
+// router.get('/health', '');
+// router.get('/metrics', '');
+
+// Orders reading
+router.get("/orders", "OrdersController.index")
+router.get("/orders/:id", "OrdersController.show")
+
+// Orders lifecycle actions
+router.post('/orders/:orderNumber/accept', 'OrdersController.accept')
+router.post('/orders/:orderNumber/process', 'OrdersController.startProcessing')
+router.post('/orders/:orderNumber/pack', 'OrdersController.pack')
+router.post('/orders/:orderNumber/ship', 'OrdersController.ship')
+router.post('/orders/:orderNumber/deliver', 'OrdersController.deliver')
+
+// Returns reading
+router.get('/returns', 'ReturnsController.index')
+router.get('/returns/:returnNumber', 'ReturnsController.show')
+
+// Returns lifecycle actions
+router.post('/returns/:returnNumber/receive', 'ReturnsController.receive')
+router.post('/returns/:returnNumber/complete', 'ReturnsController.complete')
 
 router.get('/', async () => {
   return {
