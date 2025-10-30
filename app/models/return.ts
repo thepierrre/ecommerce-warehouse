42996670695ce5b70222a1ca816831df34f9033d
Piotr Owczarczyk <piotr.owczarczyk98@gmail.com>
2025-10-30 10:38:23 +0100

    add return, return reason and return status models

---
+++ app/models/return.ts
@@ -0,0 +1,47 @@
+import { BaseModel, column } from "@adonisjs/lucid/orm"
+import { DateTime } from "luxon"
+import type { OrderStatus } from "../types/order-status.js"
+import { OrderItem } from "../types/order-item.js";
+
+export default class Return extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare returnNumber: string;
+
+  @column()
+  declare orderNumber: string;
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoUpdate: true })
+  declare lastUpdatedAt: DateTime | null
+
+  @column.dateTime()
+  declare receivedAt: DateTime | null
+
+  @column.dateTime()
+  declare inspectedAt: DateTime | null
+
+  @column.dateTime()
+  declare completedAt: DateTime | null
+
+  @column()
+  declare conditionSummary: ReturnConditionSummary | null
+
+  @column()
+  declare notes: string | null
+
+  @column()
+  declare totalPrice: number;
+
+  @column()
+  declare status: OrderStatus;
+
+  @column()
+  declare items: OrderItem[]
+}
+
+export type ReturnConditionSummary = 'OK' | 'DAMAGED' | 'MIXED';
---
+++ app/types/return-status.ts
@@ -0,0 +1,11 @@
+export const ReturnStatuses = [
+    "AWAITING",
+    "IN_TRANSIT",
+    "RECEIVED",
+    "INSPECTED",
+    "ACCEPTED",
+    "REJECTED",
+    "PARTIALLY_ACCEPTED",
+] as const;
+
+export type ReturnStatus = typeof ReturnStatuses[number];
